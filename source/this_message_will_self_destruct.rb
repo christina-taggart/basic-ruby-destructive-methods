563fac5910fceb452a561cb4f2880439df92beab
Apprentice <apprentice@dbc27.local>
2014-01-07 20:29:20 -0800

    Release 0: destroys message non-destructively.

--- source/this_message_will_self_destruct.rb
+++ source/this_message_will_self_destruct.rb
@@ -1,12 +1,17 @@
 def destroy_message(string)
   #TODO: return the part a string without the message
+  if string.include?(":")
+    string.match(/(.+:)( .+)/){$1}
+  else
+    string
+  end
 end
 
 def destroy_message!(string)
   #TODO: remove the message from string destructively!
 end
 
-# Driver code... 
+# Driver code...
 string = "this message will self-destruct: you can't hug every cat"
 original_string = string.dup
 puts destroy_message(string) == "this message will self-destruct:"
