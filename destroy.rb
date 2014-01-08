5227c9c9a0079d7153d2ab5670d695aa68935272
Apprentice <apprentice@dbc26.local>
2014-01-07 16:46:07 -0800

    Added destructive method challenge

---
+++ destroy.rb
@@ -0,0 +1,30 @@
+def destroy_message(string)
+  string.include?(":") ?string.gsub(string, string[0..(string.index(":"))]) : string
+end
+
+def destroy_message!(string)
+  string.include?(":") ?string.gsub!(string, string[0..(string.index(":"))]) : nil
+end
+
+
+# Driver code...
+string = "this message will self-destruct: you can't hug every cat"
+original_string = string.dup
+puts destroy_message(string) == "this message will self-destruct:"
+puts string == original_string # we shouldn't modify the string passed to destroy_message
+
+string = "this has no message"
+original_string = string.dup
+puts destroy_message(string) == string
+puts string == original_string # we shouldn't modify the string passed to destroy_message
+
+string = "this message will self-destruct: you can't hug every cat"
+original_string = string.dup
+destroy_message!(string)
+puts string == "this message will self-destruct:"
+puts string != original_string
+
+string = "this has no message"
+result = destroy_message!(string)
+puts result.nil?
+puts string == string
